699487da7437a5d6002546b8590d2360e20a3e5b
Nikos Koukis <nickkouk@gmail.com>
2019-11-18 14:36:12 +0000

    Fix install-plugin jira link

--- install-plugin.sh
+++ install-plugin.sh
@@ -65,7 +65,7 @@ then
     rm -rf "$PLUGIN_DIR"
 fi
 announce "Cloning and installing jira -> $PLUGIN_DIR"
-git clone "https://github.com/bergercookie/jira" "$PLUGIN_DIR"
+git clone "https://github.com/bergercookie/jira-albert-plugin" "$PLUGIN_DIR"
 announce "Installed jira -> $PLUGIN_DIR"
 
 announce "Plugin ready - Enable it from the Albert settings"
