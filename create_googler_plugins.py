c9ea76ca0ca3ea1b909af4b79b64b9c873a8ce83
Nikos Koukis <nikolaos@slamcore.com>
2020-02-15 18:05:47 +0000

    Add search_devhints

--- create_googler_plugins.py
+++ create_googler_plugins.py
@@ -75,6 +75,7 @@ custom_plugins = {
     "search_pydocs": {"googler_at": "docs.python.org", "trigger": "pydocs"},
     "search_scipy": {"googler_at": "docs.scipy.org", "trigger": "sp"},
     "search_scihub": {"googler_at": "sci-hub.tw", "trigger": "sci"},
+    "search_devhints": {"googler_at": "devhints.io", "trigger": "dev"},
 }
 
 
