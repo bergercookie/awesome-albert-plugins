b357117547871b64d28bee2fff262de10b277410
uztnus <uztnus@gmail.com>
2023-01-14 14:46:47 +0200

    fixed the pulse control plugin

--- plugins/pulse_control/__init__.py
+++ plugins/pulse_control/__init__.py
@@ -8,57 +8,74 @@ from typing import Dict, List, Union
 from fuzzywuzzy import process
 from pulsectl import Pulse, pulsectl
 
-import albert as v0
+
+from albert import *
+
+md_iid = "0.5"
+md_version = "1.2"
+#md_id = "overwrite"
+md_name = "PulseAudio - Set I/O Audio devices and Profile"
+md_description = "pulse audio handler"
+md_license = "BSD-2"
+md_url = "https://github.com/bergercookie/awesome-albert-plugins/blob/master/plugins//pulse_control"
+md_maintainers = "Nikos Koukis"
+md_lib_dependencies = ["pulsectl"]
+
 
 pulse_lock = Lock()
-
-__title__ = "PulseAudio - Set I/O Audio devices and Profile"
-__version__ = "0.4.0"
-__triggers__ = "p "
-__authors__ = "Nikos Koukis"
-__homepage__ = (
-    "https://github.com/bergercookie/awesome-albert-plugins/blob/master/plugins//pulse_control"
-)
 
 src_icon_path = str(Path(__file__).parent / "source")
 sink_icon_path = str(Path(__file__).parent / "sink")
 config_icon_path = str(Path(__file__).parent / "configuration")
 
-cache_path = Path(v0.cacheLocation()) / "pulse_control"
-config_path = Path(v0.configLocation()) / "pulse_control"
-data_path = Path(v0.dataLocation()) / "pulse_control"
+cache_path = Path(cacheLocation()) / "pulse_control"
+config_path = Path(configLocation()) / "pulse_control"
+data_path = Path(dataLocation()) / "pulse_control"
 dev_mode = True
 
 pulse = Pulse("albert-client")
 
 
-# plugin main functions -----------------------------------------------------------------------
+class ClipAction(Action):
+    def __init__(self, name, copy_text):
+        super().__init__(name, name, lambda: setClipboardText(copy_text))
+
+class FuncAction(Action):
+    def __init__(self, name, command):
+        super().__init__(name, name, command)
 
 
-def initialize():
-    """Called when the extension is loaded (ticked in the settings) - blocking."""
-
-    # create plugin locations
-    for p in (cache_path, config_path, data_path):
-        p.mkdir(parents=False, exist_ok=True)
 
 
-def finalize():
-    pass
+class Plugin(QueryHandler):
+    def id(self):
+        return __name__
+
+    def name(self):
+        return md_name
+
+    def description(self):
+        return md_description
+
+    def defaultTrigger(self):
+        return 'p '
+
+    def initialize(self):
+        """Called when the extension is loaded (ticked in the settings) - blocking."""
+
+        # create plugin locations
+        for p in (cache_path, config_path, data_path):
+            p.mkdir(parents=False, exist_ok=True)
 
 
-def handleQuery(query) -> list:
-    """Hook that is called by albert with *every new keypress*."""  # noqa
-    results = []
+    def finalize(self):
+        pass
 
-    if query.isTriggered:
+    def handleQuery(self, query) -> list:
+        """Hook that is called by albert with *every new keypress*."""  # noqa
+        results = []
+
         try:
-            query.disableSort()
-
-            results_setup = setup(query)
-            if results_setup:
-                return results_setup
-
             query_str = query.string.strip()
 
             # avoid racing conditions when multiple queries are running simultaneously (i.e,
@@ -72,9 +89,9 @@ def handleQuery(query) -> list:
             pulse_lock.release()
 
             if not query_str:
-                results.extend(render_noargs(sources_sinks, cards))
+                results.extend(self.render_noargs(query, sources_sinks, cards))
             else:
-                results.extend(render_search(sources_sinks, cards, query_str))
+                results.extend(self.render_search(sources_sinks, cards, query))
 
         except Exception:  # user to report error
             if dev_mode:  # let exceptions fly!
@@ -83,134 +100,128 @@ def handleQuery(query) -> list:
 
             results.insert(
                 0,
-                v0.Item(
-                    id=__title__,
-                    icon=None,
+                Item(
+                    id=self.name(),
+                    icon=[],
                     text="Something went wrong! Press [ENTER] to copy error and report it",
                     actions=[
-                        v0.ClipAction(
-                            f"Copy error - report it to {__homepage__[8:]}",
+                        ClipAction(
+                            f"Copy error - report it to {md_url[8:]}",
                             f"{traceback.format_exc()}",
                         )
                     ],
                 ),
             )
 
-    return results
+        query.add(results)
 
+    def render_noargs(self,
+        query,
+        sources_sinks: List[Union[pulsectl.PulseSourceInfo, pulsectl.PulseSinkInfo]],
+        cards: List[pulsectl.PulseCardInfo],
+    ) -> List[Item]:
+        """Display current source, sink and card profiles."""
+        results = []
 
-def is_sink(s):
-    return isinstance(s, pulsectl.PulseSinkInfo)
+        # active port for sources, sinks ----------------------------------------------------------
+        for s in sources_sinks:
+            # discard if it doesn't have any ports
+            if s.port_active is None:
+                continue
 
+            icon = sink_icon_path if is_sink(s) else src_icon_path
 
-def render_noargs(
-    sources_sinks: List[Union[pulsectl.PulseSourceInfo, pulsectl.PulseSinkInfo]],
-    cards: List[pulsectl.PulseCardInfo],
-) -> List[v0.Item]:
-    """Display current source, sink and card profiles."""
-    results = []
-
-    # active port for sources, sinks ----------------------------------------------------------
-    for s in sources_sinks:
-        # discard if it doesn't have any ports
-        if s.port_active is None:
-            continue
-
-        icon = sink_icon_path if is_sink(s) else src_icon_path
-
-        # fill actions
-        actions = [
-            v0.FuncAction(p.description, lambda s=s, p=p: pulse.port_set(s, p))
-            for p in s.port_list
-        ]
-
-        results.append(
-            v0.Item(
-                id=__title__,
-                icon=icon,
-                text=s.port_active.description,
-                subtext=s.description,
-                completion=__triggers__,
-                actions=actions,
-            )
-        )
-
-    # active profile for each sound card ------------------------------------------------------
-    for c in cards:
-        actions = [
-            v0.FuncAction(
-                prof.description, lambda c=c, prof=prof: pulse.card_profile_set(c, prof)
-            )
-            for prof in c.profile_list
-        ]
-
-        results.append(
-            v0.Item(
-                id=__title__,
-                icon=config_icon_path,
-                text=c.profile_active.description,
-                subtext=c.name,
-                completion=__triggers__,
-                actions=actions,
-            )
-        )
-
-    return results
-
-
-def render_search(
-    sources_sinks: List[Union[pulsectl.PulseSourceInfo, pulsectl.PulseSinkInfo]],
-    cards: List[pulsectl.PulseCardInfo],
-    query_str: str,
-) -> List[v0.Item]:
-    results = []
-
-    # sinks, sources
-    search_str_to_props: Dict[str, list] = {
-        p.description: [
-            sink_icon_path if is_sink(s) else src_icon_path,
-            s.description,
-            lambda s=s, p=p: pulse.port_set(s, p),
-        ]
-        for s in sources_sinks
-        for p in s.port_list
-    }
-
-    # profiles
-    search_str_to_props.update(
-        {
-            prof.description: [
-                config_icon_path,
-                f"Profile | {c.name}",
-                lambda c=c, prof=prof: pulse.card_profile_set(c, prof),
+            # fill actions
+            actions = [
+                FuncAction(p.description, lambda s=s, p=p: pulse.port_set(s, p))
+                for p in s.port_list
             ]
-            for c in cards
-            for prof in c.profile_list
-        }
-    )
 
-    # add albert items
-    matched = process.extract(query_str, list(search_str_to_props.keys()), limit=10)
-    for m in [elem[0] for elem in matched]:
-        icon = search_str_to_props[m][0]
-        subtext = search_str_to_props[m][1]
-        action = v0.FuncAction(m, search_str_to_props[m][2])
-
-        results.append(
-            v0.Item(
-                id=__title__,
-                icon=icon,
-                text=m,
-                subtext=subtext,
-                completion=" ".join([__triggers__, query_str]),
-                actions=[action],
+            results.append(
+                Item(
+                    id=self.name(),
+                    icon=[icon],
+                    text=s.port_active.description,
+                    subtext=s.description,
+                    completion=query.trigger,
+                    actions=actions,
+                )
             )
+
+        # active profile for each sound card ------------------------------------------------------
+        for c in cards:
+            actions = [
+                FuncAction(
+                    prof.description, lambda c=c, prof=prof: pulse.card_profile_set(c, prof)
+                )
+                for prof in c.profile_list
+            ]
+
+            results.append(
+                Item(
+                    id=self.name(),
+                    icon=[config_icon_path],
+                    text=c.profile_active.description,
+                    subtext=c.name,
+                    completion=query.trigger,
+                    actions=actions,
+                )
+            )
+
+        return results
+
+
+    def render_search(self,
+        sources_sinks: List[Union[pulsectl.PulseSourceInfo, pulsectl.PulseSinkInfo]],
+        cards: List[pulsectl.PulseCardInfo],
+        query,
+    ) -> List[Item]:
+        results = []
+
+        # sinks, sources
+        search_str_to_props: Dict[str, list] = {
+            p.description: [
+                sink_icon_path if is_sink(s) else src_icon_path,
+                s.description,
+                lambda s=s, p=p: pulse.port_set(s, p),
+            ]
+            for s in sources_sinks
+            for p in s.port_list
+        }
+
+        # profiles
+        search_str_to_props.update(
+            {
+                prof.description: [
+                    config_icon_path,
+                    f"Profile | {c.name}",
+                    lambda c=c, prof=prof: pulse.card_profile_set(c, prof),
+                ]
+                for c in cards
+                for prof in c.profile_list
+            }
         )
 
-    return results
+        # add albert items
+        matched = process.extract(query.string, list(search_str_to_props.keys()), limit=10)
+        for m in [elem[0] for elem in matched]:
+            icon = search_str_to_props[m][0]
+            subtext = search_str_to_props[m][1]
+            action = FuncAction(m, search_str_to_props[m][2])
 
+            results.append(
+                Item(
+                    id=self.name(),
+                    icon=[icon],
+                    text=m,
+                    subtext=subtext,
+                    completion=" ".join([query.trigger, query.string]),
+                    actions=[action],
+                )
+            )
 
-# supplementary functions ---------------------------------------------------------------------
+        return results
+
 
 
 def get_as_subtext_field(field, field_title=None) -> str:
@@ -226,26 +237,6 @@ def get_as_subtext_field(field, field_title=None) -> str:
 
     return s
 
+def is_sink(s):
+    return isinstance(s, pulsectl.PulseSinkInfo)
 
-def save_data(data: str, data_name: str):
-    """Save a piece of data in the configuration directory."""
-    with open(config_path / data_name, "w") as f:
-        f.write(data)
-
-
-def load_data(data_name) -> str:
-    """Load a piece of data from the configuration directory."""
-    with open(config_path / data_name, "r") as f:
-        data = f.readline().strip().split()[0]
-
-    return data
-
-
-def setup(query):
-    """Setup is successful if an empty list is returned.
-
-    Use this function if you need the user to provide you data
-    """
-
-    results = []
-    return results
